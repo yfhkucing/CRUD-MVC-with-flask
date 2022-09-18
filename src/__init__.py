99328ff597094f4a007b1b8100a98a903515ea4b
yusuf <af003yusuf@gmail.com>
2022-09-18 11:39:39 +0700

    .

--- src/__init__.py
+++ src/__init__.py
@@ -15,7 +15,7 @@ def create_app(test_config= None):
     if test_config is None:
         app.config.from_mapping( 
             SECRET_KEY=os.environ.get("kucing"),
-            SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMU_DB_URI")
+            SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DB_URI")
             )
     
     else:
