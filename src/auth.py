f60a59f9a476a276c942fa0fff1e6b57c04fa141
yusuf <af003yusuf@gmail.com>
2022-09-24 10:24:38 +0700

    .

--- src/auth.py
+++ src/auth.py
@@ -20,6 +20,8 @@ def login():
         is_pass_correct = check_password_hash(user.password, password)
         #cek kalo passwordnya bener
         if is_pass_correct :
+            #ini token buat login, pesannya terenkripsi
+            #cuma bisa dibaca mesin
             refresh = create_refresh_token(identity=user.id)
             access = create_access_token(identity=user.id)
 
